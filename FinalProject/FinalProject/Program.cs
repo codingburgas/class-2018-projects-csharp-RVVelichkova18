f4ef47dd4ffadbfe8a6d11792ddbb4f31eae7068
RVVelichkova18 <RVVelichkova18@codingburgas.bg>
2022-06-13 23:50:41 +0300

    Implement Delete user

--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -6,27 +6,54 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FinalProject
 {
+    class User
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public enum Role
+        {
+            Administrator,
+            Expert,
+            RegularUser
+        }
+
+        interface IUserService
+        {
+
+            int AddUser(User user);
+            bool UpdateUser(User user);
+            bool RemoveUserById(int id);
+            User GetUserById(int id);
+            List<User> GetUser();
+        }
+    }
     class Program
     {
         const string _connectionString = @"Data Source=.\SQLExpress;" +
        @"Initial Catalog=final-projects-csharp-database;Integrated Security=True;";
-        static bool DeleteCustomerById(int id)
+        static bool DeleteUserById(int id)
         {
-            string query = $"DELETE FROM sales.customers WHERE customer_id = @CustomerId ";
+            string query = $"DELETE FROM users WHERE Id = @Id ";
             using (SqlConnection connection = new SqlConnection(_connectionString))
             using (SqlCommand command = new SqlCommand(query, connection))
             {
-                command.Parameters.Add("@CustomerId", System.Data.SqlDbType.Int).Value = id;
+                command.Parameters.Add("@Id", System.Data.SqlDbType.Int).Value = id;
                 connection.Open();
                 Console.WriteLine("Connected!");
                 return command.ExecuteNonQuery() == 1;
             } // Disconnects from the SQL server
         }
 
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool wasDeleted = DeleteUserById(1);
+            Console.WriteLine($"Was deleted: {wasDeleted}");
         }
     }
 }
- 
